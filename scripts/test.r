116317be1c52943a650c4754c9baab43d5961edd
Gabriel Ashkenazi <gabriel.ashkenazi1@gmail.com>
2022-11-22 20:00:54 -0500

    Backend runs r-script

--- scripts/test.r
+++
@@ -1,8 +0,0 @@
-# idk what this does but it prints permutations of the inputted string
-
-needs(magrittr)
-set.seed(512)
-do.call(rep, input) %>% 
-  strsplit(NULL) %>% 
-  sapply(sample) %>% 
-  apply(2, paste, collapse = "")
